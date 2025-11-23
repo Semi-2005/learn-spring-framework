2b57b198bbf22e1ddff8778d3cbcc64c5eaa9997
Semi-2005 <kazarsemi0@gmail.com>
2025-11-23 18:17:36 +0300

    30rd lesson updates

--- src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -16,13 +16,14 @@ public class App02HelloWorldSpring {
 		
 		System.out.println(context.getBean("person"));
 		
-		System.out.println(context.getBean("adress2"));
+		System.out.println(context.getBean("address"));
 		
 		System.out.println(context.getBean("person2MethodCall"));
 		
 		System.out.println(context.getBean("person3Parameter"));
+
         System.out.println(context.getBean("person4Parameter"));
-        System.out.println(context.getBean("person5Parameter"));
+
 		
 		
 		
--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,55 +3,72 @@ package com.in28minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name , int age,Adress adress) {};
+record Person(String name, int age, Address address) {
+};
 
-record Adress(String street,String city) {};
+record Address(String street, String city) {
+};
 
 
 @Configuration
 public class HelloWorldConfiguration {
-	
-	
-	@Bean
-	public  String name() {
-		return "Semi";
-	}
 
-	@Bean
-	public  int age() {
-		return 20;
-	}
-	
-	
-	public  String Street() {
-		return "Baker Street";
-	}
-	public  String City() {
-		return "London";
-	}
-	
-	@Bean
-	public Person person() {
-		return new Person("Ali",18,new Adress("Besiktas","Istanbul"));
-	}
-	
-	@Bean( name = "adress2")
-	public Adress adress() {
-		return new Adress("Baker Street","London");
-	}
-	
 
-	
-	
-	@Bean 
-	public Person person2MethodCall() {
-		return new Person(name(),age(),adress());
-	}
-	
-	@Bean
-	public Person person3Parameter(String name , int age , Adress adress ) {
-		return new Person(name,age,adress);
-	}
-	
-	
+    @Bean
+    public String name() {
+        return "Semi";
+    }
+
+    @Bean(name = "Name2")
+    public String name2() {
+        return "Zeynep";
+    }
+
+    @Bean
+    public int age() {
+        return 20;
+    }
+
+
+    public String Street() {
+        return "Baker Street";
+    }
+
+    public String City() {
+        return "London";
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Ali", 18, new Address("Besiktas", "Istanbul"));
+    }
+
+    @Bean(name = "address")
+    public Address address() {
+        return new Address("Baker Street", "London");
+    }
+
+
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("Baker Street", "Sivas");
+    }
+
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name2(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameter(String name, int age, Address address3) {
+        return new Person(name, age, address3);
+    }
+
+    @Bean
+    public Person person4Parameter(String name, int age, Address address) {
+        return new Person(name, age, address);
+    }
+
+
 }
